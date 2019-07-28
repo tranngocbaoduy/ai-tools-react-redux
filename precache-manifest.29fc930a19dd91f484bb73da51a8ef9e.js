6b2384e60a99acdf76fe58d60540c9a8a7ca0ad8
tranngocbaoduy <tranngocbaoduy88@gmail.com>
2019-07-28 20:15:13 +0700

    Updates

---
+++ precache-manifest.29fc930a19dd91f484bb73da51a8ef9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a4d8f3eda3184a7cbef",
+    "url": "/myapp/static/css/main.b70eea5b.chunk.css"
+  },
+  {
+    "revision": "4a4d8f3eda3184a7cbef",
+    "url": "/myapp/static/js/main.67badcea.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "f190538bd2d5a7fbb6a8",
+    "url": "/myapp/static/js/2.1cdd4998.chunk.js"
+  },
+  {
+    "revision": "9c6d8216ee966bccd5cdc68cee121903",
+    "url": "/myapp/static/media/LemonJelly.9c6d8216.ttf"
+  },
+  {
+    "revision": "d4d3b88b369b077a8379ead8564d33e1",
+    "url": "/myapp/index.html"
+  }
+];
